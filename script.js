01b42701407bfe67849096f7c3f3c1335adf96c3
Paul Gross <p.gross6408@gmail.com>
2025-06-07 17:38:28 +0200

    feat: add image overlay on click with toggle open/close functionality

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 let gallery = [
-    './assets/img/gallery/night-city1.png',
-    './assets/img/gallery/night-city2.png',
-    './assets/img/gallery/night-city3.png',
-    './assets/img/gallery/night-city4.png',
-    './assets/img/gallery/night-city5.png',
-    './assets/img/gallery/night-city6.png',
-    './assets/img/gallery/night-city7.png',
-    './assets/img/gallery/night-city8.png',
-    './assets/img/gallery/night-city9.png',
-]
+  "./assets/img/gallery/night-city1.png",
+  "./assets/img/gallery/night-city2.png",
+  "./assets/img/gallery/night-city3.png",
+  "./assets/img/gallery/night-city4.png",
+  "./assets/img/gallery/night-city5.png",
+  "./assets/img/gallery/night-city6.png",
+  "./assets/img/gallery/night-city7.png",
+  "./assets/img/gallery/night-city8.png",
+  "./assets/img/gallery/night-city9.png",
+];
 
-function init(){
-    let contentRef = document.getElementById("gallery-content");
-    contentRef.innerHTML = "";
-    for (let index = 0; index < gallery.length; index++) {
-        contentRef.innerHTML += `<img src=${gallery[index]} />`
-    }
+function render() {
+  let contentRef = document.getElementById("gallery-content");
+  contentRef.innerHTML = "";
+  for (let index = 0; index < gallery.length; index++) {
+    contentRef.innerHTML += getImgTemplate(index);
+  }
+}
+
+function getImgTemplate(index) {
+  return /*html*/ `
+        <img onclick="toggleOverlay(${index})" src=${gallery[index]} />
+    `;
+}
+
+function toggleOverlay(index) {
+  let overlayRef = document.getElementById("overlay");
+  overlayRef.classList.toggle("d-none");
+  let overlayContentRef = document.getElementById("overlay-content");
+  overlayContentRef.innerHTML = "";
+  overlayContentRef.innerHTML += /*html*/ `
+    <img src="${gallery[index]}">
+  `;
 }
